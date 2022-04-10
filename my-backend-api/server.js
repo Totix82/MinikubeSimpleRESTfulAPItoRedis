721597fbf772ffab185f4e1897759ed23212c5eb
Totix82 <Totix82@users.noreply.github.com>
2022-04-10 17:42:55 +0200

    Add files via upload

---
+++ my-backend-api/server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require("express");
+const app = express();
+
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({
+    id,
+    name: `John Doe #${id}`
+  });
+});
+
+app.listen(80, () => {
+  console.log("Server running on port 80");
+});
